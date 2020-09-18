5dfd76129e408dd52ec3784454c3dc5768e45b80
Dobai Zsolt <dobaizsolt97@gmail.com>
2020-09-18 16:26:00 +0300

    new images

--- src/configs/firebase.js
+++ src/configs/firebase.js
@@ -1,11 +1,12 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyCcbZMkwd4ZV_1DiH0R3TZFCg9GKaIMeoM",
-    authDomain: "react-sirluggia-shop.firebaseapp.com",
-    databaseURL: "https://react-sirluggia-shop.firebaseio.com",
-    projectId: "react-sirluggia-shop",
-    storageBucket: "react-sirluggia-shop.appspot.com",
-    messagingSenderId: "184253035461",
-    appId: "1:184253035461:web:20fe5c173c7675c74c51ac"
+  apiKey: "AIzaSyBCqdYaCQjsmaQnQQe8zKnAu-DY5ja0AnI",
+  authDomain: "react-shop-27d78.firebaseapp.com",
+  databaseURL: "https://react-shop-27d78.firebaseio.com",
+  projectId: "react-shop-27d78",
+  storageBucket: "react-shop-27d78.appspot.com",
+  messagingSenderId: "31747840830",
+  appId: "1:31747840830:web:68afa50a1177b6bacc2a6f",
+  measurementId: "G-F1ZNBGYKE4",
 };
 
 export default firebaseConfig;
